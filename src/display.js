22b5e232f5062daf498e0345ac5a7bb96d09bdb2
nedimburo <nedimb9402@gmail.com>
2023-10-11 14:32:06 +0200

    Added basic validation to form and made style updates

--- src/display.js
+++ src/display.js
@@ -16,6 +16,12 @@ export default function initializeWebsite(){
     let submitButton=document.getElementById("submit-location-button");
     submitButton.addEventListener("click", ()=>{
         let inputLocation=document.getElementById("location-input").value;
-        displayController.getLocationData(inputLocation);
+        if (inputLocation==""){
+            let warningMessageBox=document.getElementById("warning-message");
+            warningMessageBox.textContent="Input field is empty. Please enter the location.";
+        }
+        else{
+            displayController.getLocationData(inputLocation);
+        }       
     });
 }
