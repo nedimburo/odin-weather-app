37725329c0df819560fccb42028a06c71580230e
nedimburo <nedimb9402@gmail.com>
2023-10-15 23:09:51 +0200

    Updated styles and small display fixes

--- src/display.js
+++ src/display.js
@@ -14,10 +14,11 @@ const displayController=(()=>{
         document.getElementById("location-name").textContent=weatherInfo.location_name;
         document.getElementById("temperature").textContent=weatherInfo.temp_c+` °C`;
         document.getElementById("condition").textContent=weatherInfo.condition;
-        document.getElementById("feelslike").textContent=weatherInfo.feelslike_c+` °C`;
+        document.getElementById("feelslike").textContent="Feels like: "+weatherInfo.feelslike_c+` °C`;
         // Weather icon
         let weatherImg=document.getElementById("weather-icon")
         weatherImg.src=imgPath;
+        weatherImg.classList.add("weather-image");
         // Day or night display
         let dayNightContainer=document.getElementById("day-night-container");
         dayNightContainer.innerHTML="";
@@ -35,10 +36,10 @@ const displayController=(()=>{
         dayNightContainer.appendChild(dayNightImg);
         dayNightContainer.appendChild(dayNightText);
         // Additional info
-        document.getElementById("wind-speed").textContent=weatherInfo.wind_kph;
+        document.getElementById("wind-speed").textContent=weatherInfo.wind_kph+" kph";
         document.getElementById("wind-direction").textContent=weatherInfo.wind_dir;
         document.getElementById("humidity").textContent=weatherInfo.humidity+" %";
-        document.getElementById("uv-index").textContent=weatherInfo.uv;
+        document.getElementById("uv-index").textContent="UV: "+weatherInfo.uv;
         // Time info
         document.getElementById("local-time").textContent="Local time: "+weatherInfo.local_time;
         document.getElementById("last-updated").textContent="Last updated: "+weatherInfo.last_updated;
@@ -51,12 +52,12 @@ const displayController=(()=>{
         celsiusButton.addEventListener("click", ()=>{
             toggleButtonClass(celsiusButton, fahrentheitButton);
             document.getElementById("temperature").textContent=weatherInfo.temp_c+` °C`;
-            document.getElementById("feelslike").textContent=weatherInfo.feelslike_c+` °C`;
+            document.getElementById("feelslike").textContent="Feels like: "+weatherInfo.feelslike_c+` °C`;
         });
         fahrentheitButton.addEventListener("click", ()=>{
             toggleButtonClass(fahrentheitButton, celsiusButton);
             document.getElementById("temperature").textContent=weatherInfo.temp_f+` °F`;
-            document.getElementById("feelslike").textContent=weatherInfo.feelslike_f+` °F`;
+            document.getElementById("feelslike").textContent="Feels like: "+weatherInfo.feelslike_f+` °F`;
         });
     };
     const toggleButtonClass=(selected, notSelected)=>{
